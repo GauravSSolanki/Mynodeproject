209293ce57a9382ecf0b6f889a21bd68ca3b3981
Gaurav Singh Solanki <gauravpanahera@gmail.com>
2023-12-26 15:42:55 +0530

    .

--- project-1pwn/index3.js
+++ project-1pwn/index3.js
@@ -1,6 +1,20 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+const express = require("express");
 
-mongoose.connect("mongodb://localhost:27017/Groot-noon")
+const app = express();
+const cors = require("cors");
+const PORT = 4100;
+
+app.use(express.json());
+app.use(cors());
+
+const connectDb = () => {
+  mongoose
+    .connect("mongodb://0.0.0.0:27017/project")
+    .then(() => console.log("mongodb conncted"))
+    .catch((err) => console.log(err));
+};
+connectDb();
 
 const ProductSchema = new mongoose.Schema({
   Name: String,
@@ -9,16 +23,30 @@ const ProductSchema = new mongoose.Schema({
   brand: String,
 });
 
-const senddata = async() => {
-  let ProductModel = new mongoose.ProductModel("products", ProductSchema);
-  const result = new ProductModel({
-    Name: "iphone",
-    price: 50000,
-    category: "Mobile",
-    brand: "iphone",
-  });
-  let abc =await result.save();
-  console.log(abc);
+// Define the model based on the schema
+const ProductModel = mongoose.model("Products", ProductSchema);
+
+const senddata = async () => {
+  try {
+    const result = await new ProductModel({
+      Name: "iphone",
+      price: 50000,
+      category: "Mobile",
+      brand: "iphone",
+    });
+    let abc =await result.save();
+    console.log(abc);
+  } catch (error) {
+    console.error("Error saving data:", error);
+  }
 };
 
 senddata();
+
+// app.listen(PORT, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("server is listning on http://localhost:" + PORT);
+//   }
+// });
