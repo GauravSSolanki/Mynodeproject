bf36804c84af583047774c179a4712123c388753
Gaurav Singh Solanki <gauravpanahera@gmail.com>
2023-12-27 23:45:04 +0530

    Update index3.js

--- project-1pwn/index3.js
+++ project-1pwn/index3.js
@@ -24,17 +24,17 @@ const ProductSchema = new mongoose.Schema({
 });
 
 // Define the model based on the schema
-const ProductModel = mongoose.model("Products", ProductSchema);
+const ProductModel = mongoose.model("products", ProductSchema);
 
 const senddata = async () => {
   try {
     const result = await new ProductModel({
-      Name: "iphone",
-      price: 50000,
+      Name: "vivo",
+      price: 22000,
       category: "Mobile",
       brand: "iphone",
     });
-    let abc =await result.save();
+    let abc = await result.save();
     console.log(abc);
   } catch (error) {
     console.error("Error saving data:", error);
@@ -42,6 +42,32 @@ const senddata = async () => {
 };
 
 senddata();
+
+const getData = async () => {
+  const ProductModel = mongoose.model("products", ProductSchema);
+  const data = await ProductModel.find({ Name: "vivo" });
+  console.log(data);
+};
+getData();
+
+const Delete = async () => {
+  const ProductModel = mongoose.model("products", ProductSchema);
+  const data = await ProductModel.deleteMany({ Name: "oppo" });
+  console.log(data);
+};
+Delete();
+
+const update = async () => {
+  const ProductModel = mongoose.model("products", ProductSchema);
+  const data = await ProductModel.updateOne(
+    { Name: "vivo" },
+    {
+      $set: { Name: "oppo", price: 19000, brand: "chineese" },
+    }
+  );
+  console.log(data);
+};
+// update();
 
 // app.listen(PORT, (err) => {
 //   if (err) {
