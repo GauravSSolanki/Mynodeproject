940717bc949fb4cb84511672a991e90b99bc5113
Gaurav Singh Solanki <gauravpanahera@gmail.com>
2024-03-28 12:34:13 +0530

    Update userControllers.js
    Notification using firebase-admin backend code api's.

--- project5-(mongodb-ref)/controllers/userControllers.js
+++ project5-(mongodb-ref)/controllers/userControllers.js
@@ -43,4 +43,50 @@ const getUserProfileController = async (req, res) => {
         console.log(error)
     }
 }
+
+// const admin = require('firebase-admin');
+// const serviceAccount = require('../configNotification.json'); // Path to your service account key JSON file
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount)
+// });
+// exports.sendNotifications = async (req, res) => {
+//     try {
+//         var { token, title, body } = req.body;
+//         // var image = req.body.thumnail_url;
+
+//         if (!title || !token || !body) {
+//             return res.status(400).json({
+//                 "status": 400,
+//                 "message": 'Title, token, and body are required in the request body.'
+//             });
+//         }
+//         //var token = 'token here';
+//         console.log(typeof (req.body.data), req.body.data);
+
+//         const message = {
+//             notification: {
+//                 title: title,
+//                 body: body,
+//                 // image: image
+//             },
+//             token: token
+//         };
+
+//         const response = await admin.messaging().send(message);
+//         console.log('Successfully sent message:', response);
+//         output = {
+//             "status": messages.STATUS_CODE_FOR_DATA_UPDATE,
+//             "message": "Successfully sent notification"
+//         }
+//         return res.status(output.status).json(output);
+//     }
+//     catch (error) {
+//         console.log("error in notifications", error);
+//         output = {
+//             "status": messages.STATUS_CODE_FOR_RUN_TIME_ERROR,
+//             "message": "catch errror"
+//         }
+//         return res.status(output.status).json(output);
+//     }
+// }
 module.exports = { registerController, getUserProfileController }
